b0648d3b166fe1ce53c0a3cf2a88ff1d7db7f124
christiangoran <christian.goran@protonmail.com>
2024-06-06 14:48:15 +0200

    Add correct functionality to tile no.12

--- src/App.jsx
+++ src/App.jsx
@@ -39,17 +39,43 @@ function randomSort(arr) {
 
 const shuffledPhrases = randomSort([...phrases]);
 
-function App() {
-  const [selectedTiles, setSelectedTiles] = useState([];)
+const bingoLanes = [
+  [1, 6, 11, 16, 21],
+  [2, 7, 12, 17, 22],
+  [3, 8, 12, 18, 23],
+  [4, 9, 14, 19, 24],
+  [5, 10, 15, 20, 25],
+  [1, 2, 3, 4, 5],
+  [6, 7, 8, 9, 10],
+  [11, 12, 13, 14, 15],
+  [16, 17, 18, 19, 20],
+  [21, 22, 23, 24, 25],
+  [1, 7, 13, 19, 25],
+  [5, 9, 13, 17, 21],
+];
 
+function App() {
+  const initialClickedTiles = Array(25).fill(false);
+  initialClickedTiles[12] = true;
+  const [clickedTiles, setClickedTiles] = useState(initialClickedTiles);
+
+  // Handles the click event on the tiles
   const handleClickedTile = (index) => {
-    set
+    if (index === 12) return;
+    setClickedTiles((prevClickedTiles) => {
+      const newClickedTiles = [...prevClickedTiles];
+      newClickedTiles[index] = !newClickedTiles[index];
+      return newClickedTiles;
+    });
   };
 
+  // Checks if the user has a bingo
 
   return (
     <div className="flex p-16 bg-indigo-950 justify-center rounded-xl">
       <div className="grid grid-cols-5 gap-x-2 gap-y-2 max-w-screen-md bg-red-900">
+        {/* Mapping through the shuffled array of phrases */}
+
         {shuffledPhrases.map((phrase, index) => {
           const centerTile =
             index === 12 ? "Conference Bingo, may the best one win!" : phrase;
@@ -57,17 +83,22 @@ function App() {
             index === 12
               ? "bg-lime-400 rounded-full"
               : "bg-yellow-300 rounded-md";
+          const clickedColor = clickedTiles[index == 12 ? centerColor : index]
+            ? "bg-blue-300 line-through text-gray-500"
+            : centerColor;
 
           return (
             <div
               key={index}
-              className={`flex items-center justify-center ${centerColor} p-4 w-36 h-36 hover:bg-red-300 transition-colors duration-300 ease-in-out`}
+              className={`flex items-center justify-center ${centerColor} ${clickedColor} p-4 w-36 h-36 hover:bg-red-300 transition-colors duration-300 ease-in-out`}
               onClick={() => handleClickedTile(index)}
             >
               <p className="text-center">{centerTile}</p>
             </div>
           );
         })}
+
+        {/* --------------End of the mapping function---------------- */}
       </div>
     </div>
   );
