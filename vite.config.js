60daf496a5a3f4daf60b219d423c79b22f7476e8
christiangoran <christian.goran@protonmail.com>
2024-06-11 22:36:40 +0200

    changes to package.json

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/bingo-app-react/", // Adjust this if necessary
   plugins: [react()],
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      // Any specific rollup options if needed
-    },
-  },
+  base: "/bingo-app-react/",
 });
