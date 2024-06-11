a355090becb9087197419a751d9cc0a828f3640b
christiangoran <christian.goran@protonmail.com>
2024-06-12 01:57:47 +0200

    update styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className="w-full md:w-2/3 pl-12">
+    <div className="w-full md:w-2/3 pl-4 md:pl-12">
       <h1 className="text-7xl text-left">Bingo</h1>
       <p className="text-4xl text-left">a game for remote teams</p>
     </div>
